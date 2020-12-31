4ced4fca15e3f6d46888ba2f767cc40819bece3a
makoto <whojimoto@gmail.com>
2020-12-31 23:29:04 +0900

    test commit

--- threeJs/gulpfile.js
+++ threeJs/gulpfile.js
@@ -2,10 +2,17 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('default', () => {
-  return (
-    gulp
-      .src('css/style.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('css'))
-  );
+  return gulp.watch('src/css/style.scss', () => {
+    return (
+      gulp
+        .src('src/css/style.scss')
+        .pipe(
+          sass({
+            outputStyle: 'expanded'
+          })
+          .on("error", sass.logError)
+        )
+        .pipe(gulp.dest('htdocs/css'))
+    );
+  });
 });
