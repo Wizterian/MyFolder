1473e7fff645a2fae414ad82e0e315ce1e918b82
makoto <whojimoto@gmail.com>
2021-01-02 22:27:33 +0900

    gulp設定

--- threeJs/gulpfile.js
+++ threeJs/gulpfile.js
@@ -1,18 +1,62 @@
 const gulp = require('gulp');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
 const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
 
-gulp.task('default', () => {
-  return gulp.watch('src/css/style.scss', () => {
-    return (
-      gulp
-        .src('src/css/style.scss')
-        .pipe(
-          sass({
-            outputStyle: 'expanded'
-          })
-          .on("error", sass.logError)
-        )
-        .pipe(gulp.dest('htdocs/css'))
-    );
+const paths = {
+  'scss' : 'src/css/**/*.scss',
+  'pug' : 'src/pug/**/*.pug',
+  'css' : 'dest/css/',
+  'html' : 'dest/',
+  'js' : 'dest/js/'
+}
+
+gulp.task('pug', () => {
+  return (
+    gulp
+      .src(paths.pug)
+      .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+      .pipe(pug({
+        pretty: true,
+        basedir: 'src/pug'
+      }))
+      .pipe(gulp.dest(paths.html))
+  );
+})
+
+gulp.task('scss', () => {
+  return (
+    gulp
+      .src(paths.scss)
+      .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(gulp.dest(paths.css))
+  )
+});
+
+gulp.task('browser-sync', () => {
+  return browserSync.init({
+    server: {
+      baseDir: 'dest/'
+    },
+    port: 8081,
+    reloadOnRestart: true
   });
 });
+
+gulp.task('reload', (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('watch', (done) => {
+  gulp.watch([paths.scss], gulp.series('scss', 'reload'))
+  gulp.watch([paths.pug], gulp.series('pug', 'reload'))
+  done();
+});
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
